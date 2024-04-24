99a7ed106f6e44fa62034e0e40b194a323eeb781
Teymur <teymurabbasov121@gmail.com>
2024-04-24 22:54:33 +0400

    remove

--- models.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('./db');
-const {DataTypes} = require('sequelize');
-const User = sequelize.define('user', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
-    chatId: {type: DataTypes.STRING, unique: true},
-    name: {type: DataTypes.STRING},
-    balance: {type: DataTypes.INTEGER, defaultValue: 0}
-})
-module.exports = User;
