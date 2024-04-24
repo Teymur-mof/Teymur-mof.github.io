0262adcff7786dd0946db0afd3aedb73ae7d530d
Teymur <teymurabbasov121@gmail.com>
2024-04-24 22:57:08 +0400

    remove 1

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-const { log } = require('console');
-const sequelize = require('./db')
-const UserModel = require('./models');
-const User = require('./models');
 
 async function start() {
-    try {
-        sequelize.authenticate()
-        sequelize.sync()
-    } catch (error) {
-        console.log('База капут');
-    }
+
     var h1_Id = document.getElementById('h1');
     var button_Id = document.getElementById('button');
     var balance = 0;
@@ -20,8 +11,7 @@ async function start() {
     h1_Id.addEventListener('mousedown', function() {
         balance++;
         h1_Id.textContent = `Всего кликов: ${balance}`;
-        const user = UserModel.findAll()
-        user.balance++
+        
     })
 }
-
+start()
