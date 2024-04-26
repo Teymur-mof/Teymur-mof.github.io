78ff4626eb3263abbf85bb1ad79d556dd4d0f58c
Teymur <teymurabbasov121@gmail.com>
2024-04-26 22:16:10 +0400

    23s

--- script.js
+++ script.js
@@ -10,6 +10,6 @@
         h1_Id.textContent = `Всего кликов: ${balance}`;
     });
     Telegram.WebApp.onEvent('mainmainButtonClicked', function(){
-        balance++
+        tg.sendData('data'); 
         //при клике на основную кнопку отправляем данные в строковом виде
     });
