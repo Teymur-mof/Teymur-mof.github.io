270b24b8d64796f0fdab24484393de03e4fb7cc3
Teymur <teymurabbasov121@gmail.com>
2024-04-27 13:50:17 +0400

    dsadasfasавававаf

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
     var tg = window.Telegram.WebApp;
     tg.MainButton.isVisible = true;
     tg.MainButton.isActive;
-    tg.MainButton.setText('Отправить');
+    tg.MainButton.setText('кликер');
     var h1_Id = document.getElementById('h1');
     var button_Id = document.getElementById('button');
     var balance = 0;
     button_Id.addEventListener('click', function() {
         balance++;
         h1_Id.textContent = `Всего кликов: ${balance}`;
+        tg.sendData('click')
     });
     Telegram.WebApp.onEvent('mainmainButtonClicked', function(){
         tg.sendData('data'); 
